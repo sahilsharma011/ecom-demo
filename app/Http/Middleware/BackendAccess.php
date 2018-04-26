c41257ed59475aa0577226c77f7cfcff6ba9f99a
Sahil Sharma <ss573@snu.edu.in>
2018-04-27 03:08:03 +0530

    backend access

--- app/Http/Middleware/BackendAccess.php
+++ app/Http/Middleware/BackendAccess.php
@@ -18,9 +18,9 @@ class BackendAccess
     public function handle($request, Closure $next)
     {
         // check the ip address
-        if (in_array($request->getClientIp(), config('site.backend.allowedIPS'))) {
+        // if (in_array($request->getClientIp(), config('site.backend.allowedIPS'))) {
             return $next($request);
-        }
+        // }
 
         return new Response("Error code: 403 => You are not allowed to access this page from your ip address of {$request->getClientIp()}", 403);
     }
